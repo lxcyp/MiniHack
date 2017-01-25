1718f43c635adda87c7d7d29916cf0084c642f83
James McCartney <Jsm123@gmail.com>
2017-01-25 15:46:29 +0000

    Added stream reading of all tweets targeted at bot username, with splitting out individual mentioned names

--- Twitter.py
+++ Twitter.py
@@ -3,9 +3,28 @@
 
 import tweepy
 import config
+import json
+from tweepy.streaming import StreamListener
 
 def getAuth():
     auth = tweepy.OAuthHandler(config.getApiKey(), config.getApiSecret())
     auth.set_access_token(config.getAccessToken(),config.getAccessSecret())
 
     return auth
+
+def createStream(Username):
+    auth = getAuth()
+    streamListener = tweepy.Stream(auth = auth, listener=MyListener())
+    streamListener.filter(track=[config.getBotUsername()])
+
+class MyListener(StreamListener):
+    def on_data(self, data):
+        decoded = json.loads(data)
+        entities = decoded['entities']
+        userMentions = entities['user_mentions']
+        for userMention in userMentions:
+            if userMention['screen_name'] != config.getBotUsername():
+                print(userMention['screen_name'])
+                # Its not us, do something with it..
+                # TODO: Call markov chain and tweet
+
