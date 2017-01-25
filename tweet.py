2cf7a3b8bc74a7fca2d0124f9b2f8ed1be3c8cd0
Jamie Luckett <jamieluckett@googlemail.com>
2017-01-25 17:43:55 +0000

    added exception handling

--- tweet.py
+++ tweet.py
@@ -7,19 +7,23 @@ auth = Twitter.getAuth()
 
 def tweetString(tweet):
     tweet = stripTweet(tweet)
+    tweet[0] = tweet[0].upper() #capitalize first letter
     """Takes a string and tweets it"""
     if len(tweet) > TWEET_LENGTH:
         print("Tweet length greater than", TWEET_LENGTH)
+
     elif len(tweet) == 0:
         print("Tweet length is 0")
+
     else:
         print("Tweeting:", tweet)
         try:
             api = tweepy.API(auth)
             api.update_status(tweet)
             logTweet(tweet)
-        except:
+        except Exception as e:
             print("Tweet failed")
+            print(str(e))
 
 def stripTweet(tweet):
     """Removes @ symbols from tweets"""
